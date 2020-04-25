f110042f9812e6d734ce16486c6f8ad147774833
Robin Arellano <robinarehen@gmail.com>
2020-04-25 19:22:31 +0200

    refactor: se implementa el cosumo del [servico-usuarios] y se crea la configuración para generar el token

---
+++ src/main/java/com/rahdevelopers/api/oauth/config/AutherizationServerConfig.java
@@ -0,0 +1,55 @@
+package com.rahdevelopers.api.oauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AutherizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("frontendapp").secret(this.passwordEncoder.encode("abc123"))
+				.scopes("read", "write").authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(3600).refreshTokenValiditySeconds(3600);
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(this.authenticationManager).tokenStore(this.tokenStore())
+				.accessTokenConverter(this.accessTokenConverter());
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(this.accessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("abc123");
+		return tokenConverter;
+	}
+
+}
---
+++ src/main/java/com/rahdevelopers/api/oauth/dto/UsuarioDto.java
@@ -0,0 +1,20 @@
+package com.rahdevelopers.api.oauth.dto;
+
+import java.util.List;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UsuarioDto {
+
+	private Long ID;
+	private String userName;
+	private String userPassword;
+	private Boolean enabled;
+	private String nombre;
+	private String apellidos;
+	private String email;
+	private List<RoleDto> roles;
+}
